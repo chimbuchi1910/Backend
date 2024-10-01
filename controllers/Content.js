0b033f24154c60e9a0db34d9f3e80892ef7e9bcf
Chimbuchi <echimbuchi33@gmail.com>
2024-10-01 04:53:14 -0700

    latest

--- controllers/Content.js
+++ controllers/Content.js
@@ -45,8 +45,59 @@ const getSingleContent = asyncHandler(async (req, res) => {
   }
 });
 
+const updateContent = asyncHandler(async (req, res) => {
+  const { id } = req.params; //where to pass your parameters and request.body
+  //const {password} =req.body    //to update password
+  //  const { isAdmin} =req.body
+  const { title, body } = req.body;
+  try {
+    const updateData = await Content.findById(id);
+    if (!updateData) {
+      res.status(700).json({
+        message: "User not found",
+      });
+    }
+    // updateData.isAdmin = isAdmin===true || updateData.isAdmin
+    updateData.title = title || updateData.title;
+    updateData.body = body || updateData.body;
+    await updateData.save();
+    res.status(200).json({
+      message: "Update Successful",
+      updateData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed To Update",
+      error,
+    });
+  }
+});
+
+const deleteContent = asyncHandler(async (req, res) => {
+  const { id } = req.params; // to pass id as a parameter
+  try {
+    const deleteRequest = await Content.findByIdAndDelete(id);
+    if (!deleteRequest) {
+      res.status(900).json({
+        message: "Content not Found",
+      });
+    }
+    res.status(300).json({
+      message: "Delete Successful",
+      deleteRequest,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: "Content not Found",
+      error,
+    });
+  }
+});
+
 module.exports = {
   getAllContents,
   postContent,
   getSingleContent,
+  updateContent,
+  deleteContent
 };
--- routes/Content.js
+++ routes/Content.js
@@ -3,9 +3,13 @@ const {
   getAllContents,
   postContent,
   getSingleContent,
+  updateContent,
+  deleteContent,
 } = require("../controllers/Content");
 const route = express.Router();
 route.get("/", getAllContents);
 route.post("/", postContent);
 route.get("/:id", getSingleContent); //for params
+route.put("/:id", updateContent);
+route.delete("/:id", deleteContent);
 module.exports = route;
